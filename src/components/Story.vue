7a66150a1d89dab212b0fcffd647a43f9b40522d
evnayd <vjane87@yahoo.com>
2021-05-24 22:33:39 +0200

    change the story text

--- src/components/Story.vue
+++ src/components/Story.vue
@@ -2,19 +2,17 @@
   <div class="story">
     <div class="story-text">
       <h2>My story:</h2>
-      <p>
-        My developer story began in 2019. It was a moment when I opened the code
-        editor for the first time. This journey is one of the hardest in life,
-        but also the most exciting one. Studying was hard for me at the
-        beginning. But now I see that this is totally ok, everyone goes through
-        it. The main thing is to be persistent and to not give up.
-      </p>
-      <p>
-        Sometimes people think programming is boring. But I disagree. I see a
-        lot of creativity in the frontend. I really love it because, after
-        writing a line of code, you immediately see the result of your work on
-        the screen. I think, It's really great and fun!
-      </p>
+      My developer story began in 2019, the moment I opened the code editor for
+      the first time. This journey has been one of the hardest in my life so
+      far, but also one of the most exciting. Studying was difficult for me at
+      the beginning. But now I know that is totally normal…everyone goes through
+      that when they start something new. The main thing is to be persistent and
+      never give up.
+      </p><p>
+      Some people think programming is boring. But I disagree. I see a lot of
+      creativity in frontend. I really love it because, after writing a line of
+      code, you immediately see the result of your work on the screen. I think
+      It's really interesting and fun!
     </div>
     <div class="story-img">
       <img alt="sreenshot of the site" src="../img/laptop.jpg" />
